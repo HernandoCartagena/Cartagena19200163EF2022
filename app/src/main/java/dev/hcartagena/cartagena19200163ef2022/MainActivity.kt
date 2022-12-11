cedf9bf807ee16e62dee918f35e9f5fec9150d41
Hernando Cartagena <hernando.cartagenasaldivar@telefonica.com>
2022-12-11 10:09:13 -0500

    Initial commit

---
+++ app/src/main/java/dev/hcartagena/cartagena19200163ef2022/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.hcartagena.cartagena19200163ef2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
